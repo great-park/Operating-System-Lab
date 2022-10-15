f112f82763bdf4909d26ba29ebc5fe835a0e6b19
great-park <cksgh1735@naver.com>
2022-10-15 17:06:52 +0900

    extend multiple thread

--- lab2/Multithreaded using the Pthread API.cpp
+++ lab2/Multithreaded using the Pthread API.cpp
@@ -1,32 +1,31 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-int sum; 
-void *runner(void *param); 
+#define NUM_THREADS 10
 
-int main(int argc, char *argv[])
-{
-	pthread t tid; /* the thread identifier */
-	pthread attr t attr; /* set of thread attributes */
+int sum; /* global data shared by the thread(s) */
+void *runner(void *param); /* threads call this function */
 
-	pthread attr init(&attr);
-	/* create the thread */
-	pthread create(&tid, &attr, runner, argv[1]);
-	
-	/* wait for the thread to exit */
-	pthread join(tid,NULL);
-	printf("sum = %d?n",sum);
+int main(int argc, char *argv[]) { /* a single thread(parent) of control begins */
+	pthread_ t workers[NUM_THREADS]; /* the thread identifier */
+	pthread_attr_t attr; /* set of thread attributes */
+
+	pthread_attr_init(&attr); /* set the default attributes of the thread */
+	pthread_create(&workers[0], &attr, runner, argv[1]); /* create a second thread */
+
+	for (int i=0; i<NUM_THREADS; i++){
+		pthread_join(workers[i], NULL); /* wait for the thread to exit */
+	}	
+
+	printf("sum = %d?n", sum);
 }
 
-/* The thread will execute in this function */
-void *runner(void *param)
-{
-	int i, upper = atoi(param);
-	sum = 0;
-	
-	for (i = 1; i <= upper; i++){
+void *runner(void *param) { /* The thread(child) will execute */
+	int i, upper=atoi(param);
+	sum=0;
+
+	for (i=1; i< upper; i++){
 		sum += i;
 	}
-		
-	pthread exit(0);
+	pthread_exit(0);
 }
